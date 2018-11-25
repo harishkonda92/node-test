bb90f14f6fb5473208401361bbb367bf3f0531c2
HARISH\Harish_K <harish@vdeliver.it>
2018-11-25 16:02:38 +0530

    remove console log

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const { mailer } = require('./sendemails');
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  console.log(process.env.password)
   res.render('index', { title: 'Express' });
 });
 
